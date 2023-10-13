6cc318af3a90054559a185bec64572591058d237
gtrpo123 <147526753+gtrpo123@users.noreply.github.com>
2023-10-13 11:26:17 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,16 +1,1 @@
-function fibonacci(num) 
-{ 
-    var num1 = 0; 
-    var num2 = 1; 
-    var sum; 
-    var i = 0; 
-    for (i = 0; i < num; i++)  
-    { 
-        sum = num1 + num2; 
-        num1 = num2; 
-        num2 = sum; 
-    } 
-    return num2; 
-} 
-document.write(fibonacci(1)); 
-document.write(fibonacci(12));
+
